886e11cb052e83203450bac9981d223689bdefee
kerirose <kerry.rose@outlook.com>
2019-08-14 21:11:51 -0400

    Include Web Service API source
    Include Web Service API source

--- AngularClassProject/src/app/app.module.ts
+++ AngularClassProject/src/app/app.module.ts
@@ -6,17 +6,86 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AddComponent } from './add/add.component';
 import { SearchComponent } from './search/search.component';
+<<<<<<< Updated upstream
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+=======
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatMenuModule} from '@angular/material/menu';
+import { NotFoundComponent } from './error/not-found/not-found.component';
+import { NserverErrorComponent } from './error/nserver-error/nserver-error.component';
+import { MatProgressBarModule } from '@angular/material';
+
+//Importing for piechart
+import { ChartsModule } from 'ng2-charts';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+
+
+
+
+
+
+
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+} from '@angular/material';
+
+
+
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     routingComponents
+=======
+    AddComponent,
+    HomeComponent,
+    SearchComponent,
+    MainNavComponent,
+    NotFoundComponent,
+    NserverErrorComponent
+
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+<<<<<<< Updated upstream
     BrowserAnimationsModule
+=======
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    LayoutModule,
+    MatMenuModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatProgressBarModule,
+    MatCheckboxModule, //new module import 1 browser working
+    ChartsModule, //piechart insert
+    HttpClientModule, //inserted for piechart asynchron update 
+
+
+    MatSelectModule
+>>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularClassProject/src/app/home/home.component.ts
+++ AngularClassProject/src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';     //inserted for piechart response
+import { HttpErrorResponse } from '@angular/common/http'; // inserted for piechart error response
+
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+  pieChartOptions = { responsive: true }
+
+  pieChartLabels = [ 'ex1', 'ex2', 'ex3', 'ex4', 'ex5']; //add piechart labels
+  
+  pieChartColor = [                                       // this add color to the 5 sections of the piechart
+      {                                                   // add more colors for each pie chart slice 'rgba(225, 161, 181, 0.9 )'
+      backgroundColor: ['rgba(30, 169, 224, 0.8)',
+    'rgba(225, 165, 0, 0.9)', 'rgba(139, 136, 136, 0.9 )',
+    'rgba(225, 161, 181, 0.9 )', 'rgba( 225, 102, 0, 0.9)']
+      }
+  ]
+  
+  
+  
+  pieChartData: any = [ { data: [] } ];                    // dynamically update piechart data
+  
+
+
+  constructor(private httpService: HttpClient) { }
 
   ngOnInit() {
+    this.httpService.get('./assets/data.json', {responseType: 'json'}).subscribe(
+      data => { this.pieChartData = data as any []; },       //allow piechart to be populated with data from array
+      (err: HttpErrorResponse) => { console.log (err.message);}
+    );
   }
+  onChartClick(event) { console.log(event); }
 
 }
